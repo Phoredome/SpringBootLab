a9b5e3043d80fac8bd5585d8825d6e457ec3570c
Eric Hagen <eboy.coltt5@gmail.com>
2021-02-18 13:19:41 -0800

    Commit generated files

---
+++ src/main/java/com/csis3275/mongolab/MongoLabApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275.mongolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csis3275/mongolab/MongoLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275.mongolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
